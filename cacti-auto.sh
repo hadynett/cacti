467e2344787f48beb509f048e3ad7fff0cce15be
hadynett <hadynett@gmail.com>
2023-04-12 05:12:44 +0700

    Update cacti-auto.sh

--- cacti-auto.sh
+++ cacti-auto.sh
@@ -23,7 +23,7 @@ fi
 ##Download required packages for cacti
 echo "cacti requires a LAMP stack as well as some required plugins we will now install the required packages"
 apt-get update
-apt-get  install -y apache2 libapache2-mod-php  rrdtool mariadb-server snmp snmpd php php-mysql  libapache2-mod-php   php-snmp php-xml php-mbstring php-json php-gd php-gmp php-zip php-ldap 
+apt-get  install -y apache2 libapache2-mod-php  rrdtool mariadb-server snmp snmpd php php-mysql  libapache2-mod-php  php-intl php-snmp php-xml php-mbstring php-json php-gd php-gmp php-zip php-ldap 
 echo "will you be using the spine poller enter 1 for yes 2 for no"
 read answer
 if [ $answer == "1" ]
